1874d9abec495563c03a250aba50b18d2250ad3a
wstrn <1533225965@qq.com>
2023-03-15 21:57:49 +0800

    add gitproxy helper script

---
+++ .gitproxy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]
+then
+	if [ "$1" == "set" ]
+	then
+		echo "setting git proxy"
+		host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
+		echo "host ip: $host_ip"
+		cmd_string="git config --global http.proxy 'socks5://$host_ip:10809';"
+		$cmd_string
+		cmd_string="git config --global https.proxy 'socks5://$host_ip:10809'"
+		$cmd_string
+		echo check:
+		git config --global --get http.proxy;git config --global --get https.proxy
+		exit 0
+
+	elif [ "$1" == "unset" ]
+	then
+		echo "canceling git proxy"
+		git config --global --unset http.proxy
+		git config --global --unset https.proxy
+		echo check:
+		git config --global --get http.proxy;git config --global --get https.proxy
+		exit 0
+
+	else
+		echo "arguement error"
+		exit 1
+	fi
+else
+	echo "usage: gitproxy [arg]"
+	echo "[arg]:"
+	echo "      set:   to enable git proxy"
+	echo "      unset: to disable git proxy"
+	exit 0
+fi
+
