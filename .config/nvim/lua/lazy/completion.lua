3da0b1f8c9f4f4df32e8a4607fd35363efb829e2
Will <wstrn66@gmail.com>
2024-10-31 03:34:13 +1100

    change completion key behavior

--- .config/nvim/lua/lazy/completion.lua
+++ .config/nvim/lua/lazy/completion.lua
@@ -1,6 +1,13 @@
+-- local has_words_before = function()
+-- 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+-- 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+-- end
 local has_words_before = function()
+	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+		return false
+	end
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
 end
 
 return {
@@ -33,7 +40,10 @@ return {
 					end,
 				},
 				config = function()
-					require("copilot_cmp").setup()
+					require("copilot_cmp").setup({
+						event = { "InsertEnter", "LspAttach" },
+						fix_pairs = true,
+					})
 				end,
 			},
 		},
@@ -70,34 +80,70 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-i>"] = cmp.mapping.select_prev_item(),
 					["<C-k>"] = cmp.mapping.select_next_item(),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
+					["<CR>"] = function(fallback)
+						if
+							not cmp.visible()
+							or not cmp.get_selected_entry()
+							or cmp.get_selected_entry().source.name == "nvim_lsp_signature_help"
+						then
+							fallback()
+						else
 							cmp.confirm({
 								behavior = cmp.ConfirmBehavior.Replace,
-								select = true,
+								-- Don't select first item on CR if nothing was selected
+								select = false,
 							})
+						end
+					end,
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
 						elseif luasnip.expand_or_jumpable() then
 							luasnip.expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
 						else
 							fallback()
 						end
 					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function()
+					["<S-Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
-							cmp.close()
+							cmp.select_prev_item()
 						elseif luasnip.jumpable(-1) then
 							luasnip.jump(-1)
-						else
+						elseif has_words_before() then
 							cmp.complete()
+						else
+							fallback()
 						end
 					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "copilot" },
-					{ name = "buffer" },
+					-- Copilot Source
+					{ name = "copilot", group_index = 2 },
+					-- Other Sources
+					{ name = "nvim_lsp", group_index = 2 },
+					{ name = "luasnip", group_index = 2 },
+					{ name = "buffer", group_index = 2 },
 				}),
+				sorting = {
+					priority_weight = 2,
+					comparators = {
+						require("copilot_cmp.comparators").prioritize,
+
+						-- Below is the default comparitor list and order for nvim-cmp
+						cmp.config.compare.offset,
+						-- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+						cmp.config.compare.exact,
+						cmp.config.compare.score,
+						cmp.config.compare.recently_used,
+						cmp.config.compare.locality,
+						cmp.config.compare.kind,
+						cmp.config.compare.sort_text,
+						cmp.config.compare.length,
+						cmp.config.compare.order,
+					},
+				},
 			})
 			-- Set configuration for specific filetype.
 			cmp.setup.filetype("gitcommit", {
